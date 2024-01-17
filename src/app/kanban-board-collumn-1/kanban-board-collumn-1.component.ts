2562d0cfeb90413349bc648674b56e000ed0b6d0
Palaptin <>
2024-01-17 08:39:37 +0100

    zwischenstand

--- src/app/kanban-board-collumn-1/kanban-board-collumn-1.component.ts
+++ src/app/kanban-board-collumn-1/kanban-board-collumn-1.component.ts
@@ -1,6 +1,4 @@
-import { Component, HostBinding, OnInit } from '@angular/core';
-import { Ticket } from '../ticket';
-import { TicketService } from '../ticket.service';
+import { Component,} from '@angular/core';
 
 @Component({
   selector: 'app-kanban-board-collumn-1',
@@ -8,18 +6,5 @@ import { TicketService } from '../ticket.service';
   styleUrl: './kanban-board-collumn-1.component.css'
 })
 
-export class KanbanBoardCollumn1Component implements OnInit{
-  tickets: Ticket[] = [];
-
-  constructor(private ticketService: TicketService){}
-
-  ngOnInit(): void {
-    this.getTickets();
-  }
-
-  getTickets(): void {
-    this.ticketService.getTickets()
-        .subscribe(tickets => this.tickets = tickets);
-      console.log(this.tickets)
-  }
+export class KanbanBoardCollumn1Component {
 }
--- src/app/kanban-board/kanban-board.component.ts
+++ src/app/kanban-board/kanban-board.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { TicketService } from '../ticket.service';
+import { Ticket } from '../ticket';
+
 
 @Component({
   selector: 'app-kanban-board',
@@ -6,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrl: './kanban-board.component.css'
 })
 export class KanbanBoardComponent {
+  constructor(private ticketService: TicketService){}
+  tickets: Ticket[][] = [[],[],[],[],[]];
+  ngOnInit(): void {
+    this.getTickets();
+  }
+
+  getTickets(): void {
+    var ticket_array: Ticket[];
+    this.ticketService.getTickets()
+        .subscribe(tickets => this.sortTickets(tickets));
+    console.log(this.tickets);
+    
+  }
+
+  sortTickets(tickets: Ticket[]): void {
+    for(let ticket of tickets){
+      console.log(ticket);
+      
+      this.tickets[ticket.state-1].push(ticket)
+    }
+
+  }
 
 }
